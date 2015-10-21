1a9bc4f349ff3f9995e6cbd86420ca634dc29f70
Mrunal Patel <mrunalp@gmail.com>
2015-10-21 12:49:10 -0400

    Verify all capabilities set or unset
    Signed-off-by: Mrunal Patel <mrunalp@gmail.com>

--- cmd/runtimetest/main.go
+++ cmd/runtimetest/main.go
@@ -111,6 +111,7 @@ func validateProcess(spec *specs.LinuxSpec, rspec *specs.LinuxRuntimeSpec) error
 
 func validateCapabilities(spec *specs.LinuxSpec, rspec *specs.LinuxRuntimeSpec) error {
 	capabilityMap := make(map[string]capability.Cap)
+	expectedCaps := make(map[capability.Cap]bool)
 	last := capability.CAP_LAST_CAP
 	// workaround for RHEL6 which has no /proc/sys/kernel/cap_last_cap
 	if last == capability.Cap(63) {
@@ -122,6 +123,12 @@ func validateCapabilities(spec *specs.LinuxSpec, rspec *specs.LinuxRuntimeSpec) 
 		}
 		capKey := fmt.Sprintf("CAP_%s", strings.ToUpper(cap.String()))
 		capabilityMap[capKey] = cap
+		expectedCaps[cap] = false
+	}
+
+	for _, ec := range spec.Linux.Capabilities {
+		cap := capabilityMap[ec]
+		expectedCaps[cap] = true
 	}
 
 	processCaps, err := capability.NewPid(1)
@@ -129,14 +136,17 @@ func validateCapabilities(spec *specs.LinuxSpec, rspec *specs.LinuxRuntimeSpec) 
 		return err
 	}
 
-	for _, ec := range spec.Linux.Capabilities {
-		cap := capabilityMap[ec]
-		if !processCaps.Get(capability.EFFECTIVE, cap) {
-			return fmt.Errorf("Expected Capability %v not set for process")
+	for _, cap := range capability.List() {
+		expectedSet := expectedCaps[cap]
+		actuallySet := processCaps.Get(capability.EFFECTIVE, cap)
+		if expectedSet != actuallySet {
+			if expectedSet {
+				return fmt.Errorf("Expected Capability %v not set for process", cap.String())
+			} else {
+				return fmt.Errorf("Unexpected Capability %v set for process", cap.String())
+			}
 		}
 	}
-
-	//TODO check that unexpected caps aren't set
 
 	return nil
 }
