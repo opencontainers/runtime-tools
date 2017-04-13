f44ca535382f2114e93ff253e051bb54e123c49f
zhouhao <zhouhao@cn.fujitsu.com>
2017-04-13 10:09:15 +0800

    validate: delete the extra defaultCaps
    Signed-off-by: zhouhao <zhouhao@cn.fujitsu.com>

--- validate/validate.go
+++ validate/validate.go
@@ -40,22 +40,6 @@ var (
 		"RLIMIT_SIGPENDING",
 		"RLIMIT_STACK",
 	}
-	defaultCaps = []string{
-		"CAP_CHOWN",
-		"CAP_DAC_OVERRIDE",
-		"CAP_FSETID",
-		"CAP_FOWNER",
-		"CAP_MKNOD",
-		"CAP_NET_RAW",
-		"CAP_SETGID",
-		"CAP_SETUID",
-		"CAP_SETFCAP",
-		"CAP_SETPCAP",
-		"CAP_NET_BIND_SERVICE",
-		"CAP_SYS_CHROOT",
-		"CAP_KILL",
-		"CAP_AUDIT_WRITE",
-	}
 )
 
 // Validator represents a validator for runtime bundle
