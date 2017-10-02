a9dbd7e322c2e57502e17711f1c926184e854c86
W. Trevor King <wking@tremily.us>
2017-10-02 15:26:00 -0700

    cmd/oci-runtime-tool: Implement --compliance-level
    The man-page entry and Bash completions for this option were added in
    4029999d (oci error: add error level and reference, 2017-03-31, #354),
    but the option was left unimplemented.  The implementation in this
    commit is very similar to the existing runtimetest implementation from
    4029999d, except we warn instead of silently skipping non-fatal spec
    violations.
    
    The warning (vs. error) on invalid level arguments follows the
    runtimetest implementation from 6316a4e0 (use released version as
    reference; improve Parse error, 2017-04-12, #354).  I'd personally
    prefer hard errors on invalid level arguments, but didn't want to
    break runtime-tools consistency over that.
    
    Signed-off-by: W. Trevor King <wking@tremily.us>

--- cmd/oci-runtime-tool/validate.go
+++ cmd/oci-runtime-tool/validate.go
@@ -3,7 +3,11 @@ package main
 import (
 	"fmt"
 
+	"github.com/hashicorp/go-multierror"
+	rfc2119 "github.com/opencontainers/runtime-tools/error"
+	"github.com/opencontainers/runtime-tools/specerror"
 	"github.com/opencontainers/runtime-tools/validate"
+	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli"
 )
 
@@ -19,6 +23,12 @@ var bundleValidateCommand = cli.Command{
 	Before: before,
 	Action: func(context *cli.Context) error {
 		hostSpecific := context.GlobalBool("host-specific")
+		complianceLevelString := context.GlobalString("compliance-level")
+		complianceLevel, err := rfc2119.ParseLevel(complianceLevelString)
+		if err != nil {
+			complianceLevel = rfc2119.Must
+			logrus.Warningf("%s, using 'MUST' by default.", err.Error())
+		}
 		inputPath := context.String("path")
 		platform := context.String("platform")
 		v, err := validate.NewValidatorFromPath(inputPath, hostSpecific, platform)
@@ -27,8 +37,20 @@ var bundleValidateCommand = cli.Command{
 		}
 
 		if err := v.CheckAll(); err != nil {
-			return err
-
+			merr, ok := err.(*multierror.Error)
+			if !ok {
+				return err
+			}
+			var validationErrors error
+			for _, err = range merr.Errors {
+				e, ok := err.(*specerror.Error)
+				if ok && e.Err.Level < complianceLevel {
+					logrus.Warn(e)
+					continue
+				}
+				validationErrors = multierror.Append(validationErrors, err)
+			}
+			return validationErrors
 		}
 		fmt.Println("Bundle validation succeeded.")
 		return nil
