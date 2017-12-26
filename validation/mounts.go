c2f774cc642f4b126252fee23e38b534fb5a4e65
Zhou Hao <zhouhao@cn.fujitsu.com>
2017-12-26 15:35:48 +0800

    validation: add mount validation
    Signed-off-by: Zhou Hao <zhouhao@cn.fujitsu.com>

--- validation/mounts.go
+++ validation/mounts.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	rspec "github.com/opencontainers/runtime-spec/specs-go"
 	"github.com/opencontainers/runtime-tools/validation/util"
 )
 
 func main() {
-	util.Skip("TODO: mounts generation options have not been implemented", "")
+	g := util.GetDefaultGenerator()
+	mount := rspec.Mount{
+		Destination: "/tmp",
+		Type:        "tmpfs",
+		Source:      "tmpfs",
+		Options: []string{
+			"nosuid",
+			"strictatime",
+			"mode=755",
+			"size=1k",
+		},
+	}
+	g.AddMount(mount)
+	err := util.RuntimeInsideValidate(g, nil)
+	if err != nil {
+		util.Fatal(err)
+	}
 }
