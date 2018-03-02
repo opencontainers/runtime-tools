0451545bb420d86ee758b93fea2c9b38e3278729
Vincent Batts <vbatts@hashbangbash.com>
2018-03-02 15:23:50 -0500

    validate: allow non-linux compatibility
    Signed-off-by: Vincent Batts <vbatts@hashbangbash.com>

--- validate/validate.go
+++ validate/validate.go
@@ -862,17 +862,6 @@ func CapValid(c string, hostSpecific bool) error {
 	return nil
 }
 
-// LastCap return last cap of system
-func LastCap() capability.Cap {
-	last := capability.CAP_LAST_CAP
-	// hack for RHEL6 which has no /proc/sys/kernel/cap_last_cap
-	if last == capability.Cap(63) {
-		last = capability.CAP_BLOCK_SUSPEND
-	}
-
-	return last
-}
-
 func envValid(env string) bool {
 	items := strings.Split(env, "=")
 	if len(items) < 2 {
