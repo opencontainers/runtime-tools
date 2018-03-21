ea512c3fa25011014c186923c1263fd0138c8eff
Zhou Hao <zhouhao@cn.fujitsu.com>
2018-03-21 11:10:26 +0800

    validation: add linux_process validation
    Signed-off-by: Zhou Hao <zhouhao@cn.fujitsu.com>

--- validation/linux_process/linux_process.go
+++ validation/linux_process/linux_process.go
@@ -10,6 +10,11 @@ func main() {
 		util.Fatal(err)
 	}
 	g.SetProcessOOMScoreAdj(500)
+	g.SetProcessNoNewPrivileges(true)
+	g.SetupPrivileged(true)
+	g.SetProcessApparmorProfile("acme_secure_profile")
+	g.SetProcessSelinuxLabel("system_u:system_r:svirt_lxc_net_t:s0:c124,c675")
+
 	err = util.RuntimeInsideValidate(g, nil)
 	if err != nil {
 		util.Fatal(err)
